56f85a96a3eecc0e12ce8710697be204af5530c1
pradeep.prabhu7@gmail.com <Sriram7@>
2018-11-15 15:04:44 +0000

    Test_0003: syntax updated

--- src/test/java/APITesting/com/org/api/weatherGetRequest.java
+++ src/test/java/APITesting/com/org/api/weatherGetRequest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.jayway.restassured.response.Response;
+import com.jayway.restassured.response.ValidatableResponse;
 
 import static com.jayway.restassured.RestAssured.*;
 
@@ -16,13 +17,13 @@ public class weatherGetRequest {
 	{
 		// Test_001 : Test_Case_01: get weather data for a city
 		
-		Response responefromget = when(). 
+		Response responsefromget = when(). 
 		get("https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22");
 		
 		// Check the status code is 200 for successful response 
-		Assert.assertEquals(responefromget.getStatusCode(), 200);
+		Assert.assertEquals(responsefromget.getStatusCode(), 200);
 		
-		if(responefromget.getStatusCode()==200)
+		if(responsefromget.getStatusCode()==200)
 		{
 			System.out.println("REST_API CALL for Test_Case_Num: Success");			
 		}
@@ -50,19 +51,21 @@ public class weatherGetRequest {
 		
 	}
 	
-	// This is for a simple get request for getting weather request by city name and how to manage the response data and verify 
+	// This is for a simple get request for getting weather request by city name and how to capture the response data in string
 	@Test
 	public void Test_0003()
 	{
 		// Test_001 : Test_Case_01: get weather data for a city
 		
-				given().
+			ValidatableResponse responsefromget = given().
 				param("q","London").
 				param("appid","b6907d289e10d714a6e88b30761fae22").
 				when().
 				get("https://samples.openweathermap.org/data/2.5/weather").
 				then().
 				assertThat().statusCode(200);
+				
+				System.out.println(responsefromget);
 		
 	}
 	
